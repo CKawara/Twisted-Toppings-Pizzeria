29d539a6460674ce02166f607e32e017e860f1a6
Catherine Kawara <ndutakawara2@gmail.com>
2022-03-20 00:50:37 +0300

    ft:cost-calculation - create a funtion to calculate pizza price

---
+++ js/index.js
@@ -0,0 +1,58 @@
+function Order(size, crust, toppings, amount, price){
+    this.size = size;
+    this.crust = crust;
+    this.toppings = toppings;
+    this.amount = amount
+    this.price = price
+}
+Order.prototype.cost = function(){ 
+// prices for different pizza sizes
+    if(this.size === "Large"){
+        var sizePrice = 1000;
+    }else if(this.size === "Medium"){
+        var sizePrice = 700;
+    }else if(this.size ==="Small"){
+        var sizePrice = 500;
+    }
+// prices for different crusts
+    if(this.crust === "Butter-Cheese"){
+        var crustPrice = 200;
+    }else if (this.crust === "Garlic-buttery-blend"){
+        var crustPrice = 180;
+    }else if (this.crust === "Gluten-free"){
+        var crustPrice = 250;
+    }else if(this.crust === "Fiery-red-pepper"){
+        var crustPrice = 150;
+    }
+// checkbox prices
+    var checkboxes = $('input[name="toppings"]:checked').length;
+    if(checkboxes <= 4){
+        if(this.size === "Large"){
+            var toppingsPrice = checkboxes * 150;
+        }else if(this.size === "Medium"){
+            var toppingsPrice = checkboxes * 100;
+        }else if(this.size ==="Small"){
+            var toppingsPrice = checkboxes * 50;
+        }
+        return sizePrice + crustPrice + toppingsPrice;
+    }else{
+        alert("You've selected more than 4 toppings!!");
+    }
+
+    
+}
+$("document").ready(function(){
+
+    $("#make-pizza").submit (function(event){
+        event.preventDefault();
+        var size = $('select#size').val();
+        var crust = $('select#crust').val(); 
+        var toppings = [];
+        $.each($('input[name="toppings"]:checked'),function(){
+            toppings.push($(this).val());
+        });
+        var amount = parseInt($('input#amount').val());
+        var pizzaOrder = new Order(size, crust, toppings, amount);
+        alert(pizzaOrder.cost() * amount);
+    })
+})
